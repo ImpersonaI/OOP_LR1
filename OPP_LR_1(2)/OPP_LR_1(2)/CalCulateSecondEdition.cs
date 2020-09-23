5e6ab556d55ad93749b444f6101125d0325701ae
ImpersonaI <43351355+ImpersonaI@users.noreply.github.com>
2020-09-23 14:41:56 +0300

    The second part of the task

---
+++ OPP_LR_1(2)/OPP_LR_1(2)/CalCulateSecondEdition.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace OPP_LR_1_2_
+{
+    class CalCulateSecondEdition
+    {
+        private static int N = 0;            
+        private double sum = 0;
+        public void InputN()
+        {                   
+
+            try
+            {
+                Console.WriteLine("Input N");
+                N = Convert.ToInt32(Console.ReadLine());
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
+
+        }
+
+        public void InputArray()
+        {
+
+            double[] a = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+
+                Console.WriteLine("Input a[{0}]", i + 1);
+                try
+                {
+
+                    a[i] = Convert.ToDouble(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
+
+                sum += a[i];
+            }
+        }
+
+        public void OutputArray()
+        {
+        
+        double[] b = new double[N];
+            for (int i = 0; i<N; i++)
+            {
+
+                b[i] = sum;
+                Console.WriteLine("Input b[" + (i + 1).ToString() + "] = " + b[i]);
+            }
+        }
+    }
+}
---
+++ OPP_LR_1(2)/OPP_LR_1(2)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OPP_LR_1_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalCulateSecondEdition dotask = new CalCulateSecondEdition();
+            dotask.InputN();
+            dotask.InputArray();
+            dotask.OutputArray();
+        }
+    }
+}
