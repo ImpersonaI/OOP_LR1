c4f3220ee40dd4dd525344803f3721152a913f57
ImpersonaI <43351355+ImpersonaI@users.noreply.github.com>
2020-09-23 14:33:07 +0300

    Add files via upload

---
+++ OOP_LR_1/OOP_LR_1/DoTask.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace OOP_LR_1
+{
+    class DoTask
+    {
+        public static void Calculate()
+        {
+
+            int N = 0;
+
+            
+             try
+            {
+                Console.WriteLine("Input N");
+                N = Convert.ToInt32(Console.ReadLine());
+               
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
+            
+
+           // Console.WriteLine("Input N");           
+            //N = Convert.ToInt32(Console.ReadLine());
+            double[] a = new double[N];
+            double sum = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("Input a[" + (i + 1).ToString() + "]");
+                try
+                {
+
+                    a[i] = Convert.ToDouble(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
+                
+                sum += a[i];
+            }
+
+            double[] b = new double[N];
+            for (int i = 0; i < N; i++)
+            {
+
+                b[i] = sum;
+                Console.WriteLine("Input b[" + (i + 1).ToString() + "] = " + b[i]);
+            }
+        }
+    }
+}
---
+++ OOP_LR_1/OOP_LR_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace OOP_LR_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DoTask.Calculate();
+
+            Console.ReadKey();
+                   
+        }
+    }
+}
